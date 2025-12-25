55b860ccd971d08f6c980425633dafa5d3b759e3
Xenia.Liubachka <bygermina@gmail.com>
2025-12-25 19:27:09 +0100

    add paths

--- src/shared/lib/svg-paths.ts
+++ src/shared/lib/svg-paths.ts
@@ -1,30 +1,135 @@
 export const pathTree = {
   path: '',
   start: { x: 400, y: 261 },
-  delay: 1.4,
-  duration: 3,
+  delay: 0.47,
+  duration: 1,
+  size: 3,
 };
 
 export const pathTree2 = {
-  path: 'M 438,947 L440,880 L470,850 L470,800 L412,723 L412,690 L385,663 L347,663 L340,655 338,645, L 340,644 L 340,644 L 339,644 L 338,644 L 338,644 L 337,644 L 336,644 L 336,644 L 335,644 L 334,644 L 334,643 L 333,643 L 332,643 L 332,643 L 331,643 L 330,643 L 330,642 L 330,642 L 330,641 L 329,641 L 329,641 L 328,641 L 328,640 L 328,640 L 328,639 L 328,639 L 327,639 L 326,639 L 326,638 L 326,637 L 326,637 L 326,636 L 326,635 L 325,635 L 325,635 L 325,634 L 324,634 L 324,633 L 324,633 L 324,632 L 324,632 L 324,631 L 324,631 L 324,630 L 324,629 L 324,629 L 324,628 L 324,627 L 324,627 L 324,626 L 324,625 L 324,625 L 324,625 L 324,624 L 325,624 L 325,623 L 326,623 L 326,623 L 326,623 L 326,622 L 326,621 L 327,621 L 327,621 L 327,620 L 327,619 L 328,619 L 328,619 L 329,619 L 330,619',
-  start: { x: 450, y: 340 },
-  delay: 1.1,
-  duration: 3,
+  path: 'M467,843 L467,785 L492,760 L492,734 L474,718 L441,717 L430,727 L404,726 L357,678 L341,678',
+  start: { x: 502, y: 305 },
+  delay: 0.33,
+  duration: 1,
 };
 
 export const pathTree3 = {
-  path: 'M 350,850 L350,790 L340,770 L340,530 L350,500 L350,400 L377,390',
-  start: { x: 490, y: 340 },
-  delay: 0.8,
-  duration: 3,
+  path: 'M493,831 L493,775 L478,759 L485,745 L485,703 L420,638 L420,614 L395,587 L363,587 L355,577',
+  start: { x: 536, y: 305 },
+  delay: 0.5,
+  duration: 1,
 };
 
 export const pathTree4 = {
-  path: '',
-  start: { x: 320, y: 258 },
-  delay: 0.5,
-  duration: 3,
+  path: 'M350,850 L350,791 L332,771 L330,670 L306,645 L306,637 L266,596 L266,563 L257,546 L228,517',
+  start: { x: 570, y: 305 },
+  delay: 0.33,
+  duration: 1,
+};
+export const pathTree5 = {
+  path: 'M350,850 L350,791  L332,771 L332,555 L294,519 L294,503 L259,468 L259,430',
+  start: { x: 570, y: 305 },
+  delay: 0.53,
+  duration: 1,
 };
 
-export const paths = [pathTree4, pathTree3, pathTree2];
+export const pathTree6 = {
+  path: 'M350,850 L350,791 L332,771 L332,540 L355,520 L355,490 L332,470 L332,400  L342,385 L330,370 L330,280',
+  start: { x: 570, y: 305 },
+  delay: 0.73,
+  duration: 1,
+};
+
+export const pathTree7 = {
+  path: 'M350,850 L350,791 L332,771 L332,540 L355,520 L355,437 L377,412 L377,405',
+  start: { x: 570, y: 305 },
+  delay: 0.93,
+  duration: 1,
+};
+
+export const pathTree8 = {
+  path: 'M350,850 L350,791 L332,771 L332,707 L357,682 L357,659  L370,653 L368,638 L410,590 L410,588 L420,580 L413,560  L410,526',
+  start: { x: 570, y: 305 },
+  delay: 1.13,
+  duration: 1,
+};
+
+export const pathTree9 = {
+  path: 'M557,858 L557,800 L530,773 L530,730 L570,691 L570,672 L617,624 L620,617 L630,611 L645,598 L664,597  ',
+  start: { x: 604, y: 305 },
+  delay: 0.17,
+  duration: 1,
+};
+
+export const pathTree10= {
+  path: 'M557,858 L557,800 L530,773 L532,733 L558,733 L572,747 L595,747 L627,713 L720,713  ',
+  start: { x: 604, y: 305 },
+  delay: 0.37,
+  duration: 1,
+};
+
+export const pathTree2Short = {
+  path: 'M467,843 L467,785 L492,760 L492,734 L474,718 L441,717',
+  start: { x: 502, y: 305 },
+  delay: 1.3,
+  duration: 0.67,
+};
+
+export const pathTree3Short = {
+  path: 'M493,831 L493,775 L478,759 L485,745 L485,703 L420,638',
+  start: { x: 536, y: 305 },
+  delay: 1.17,
+  duration: 0.67,
+};
+
+export const pathTree4Short = {
+  path: 'M350,850 L350,791 L332,771 L330,670 L306,645',
+  start: { x: 570, y: 305 },
+  delay: 1.33,
+  duration: 0.67,
+};
+
+export const pathTree5Short = {
+  path: 'M350,850 L350,791 L332,771 L332,555 L294,519',
+  start: { x: 570, y: 305 },
+  delay: 1.53,
+  duration: 0.67,
+};
+
+export const pathTree6Short = {
+  path: 'M350,850 L350,791 L332,771 L332,540 L355,520 L355,490',
+  start: { x: 570, y: 305 },
+  delay: 1.73,
+  duration: 0.67,
+};
+
+export const pathTree7Short = {
+  path: 'M350,850 L350,791 L332,771 L332,540 L355,520',
+  start: { x: 570, y: 305 },
+  delay: 1.93,
+  duration: 0.67,
+};
+
+export const pathTree8Short = {
+  path: 'M350,850 L350,791 L332,771 L332,707 L357,682 L357,659',
+  start: { x: 570, y: 305 },
+  delay: 2.13,
+  duration: 0.67,
+};
+
+export const pathTree9Short = {
+  path: 'M557,858 L557,800 L530,773 L530,730 L570,691',
+  start: { x: 604, y: 305 },
+  delay: 0.57,
+  duration: 0.67,
+};
+
+export const pathTree10Short = {
+  path: 'M557,858 L557,800 L530,773 L532,733 L558,733',
+  start: { x: 604, y: 305 },
+  delay: 0.77,
+  duration: 0.67,
+};
+
+export const paths = [ pathTree10, pathTree10Short, pathTree9, pathTree9Short, pathTree8, pathTree8Short, pathTree7, pathTree7Short, pathTree6, pathTree6Short, pathTree5, pathTree5Short, pathTree4, pathTree4Short, pathTree3, pathTree3Short, pathTree2, pathTree2Short];
 
